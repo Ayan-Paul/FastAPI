bc3a012cdfee60780ca7a641b7743eca04431041
Ayan-Paul <paul.ayan@outlook.com>
2026-02-15 22:16:56 +0530

    Modify directory path

--- routers/auth.py
+++ routers/auth.py
@@ -44,7 +44,7 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-templates = Jinja2Templates(directory="TodoApp/templates")
+templates = Jinja2Templates(directory="templates")
 
 ### Pages ###
 @router.get("/login-page")
